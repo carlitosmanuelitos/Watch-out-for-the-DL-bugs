ee8f3c555c3d1ba022fa845e316fc26fbe9badf5
carlitosmanuelitos <miguelglopes@gmail.com>
2023-10-29 02:57:43 +0100

    updated data_tsa

---
+++ 1/data_preprocessor.py
@@ -0,0 +1,236 @@
+
+
+
+# Other settings
+from IPython.display import display, HTML
+import os, warnings, logging
+warnings.filterwarnings('ignore')
+pd.set_option('display.float_format', '{:.3f}'.format)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s [%(levelname)s] - %(message)s')
+logger = logging.getLogger(__name__)
+
+class UnifiedDataPreprocessor:
+    """ 
+    UnifiedDataPreprocessor is responsible for preprocessing time series data.
+    It performs actions like data splitting, normalization, reshaping, and sequence generation.
+    
+    Attributes:
+        data (pd.DataFrame): Original time series data.
+        target_column (str): Target column for preprocessing.
+        logger (logging.Logger): Logger for tracking operations and debugging.
+        transformations (list): List of applied transformations.
+    """
+    
+    def __init__(self, df, target_column, logger=None):
+        self.data = df.copy()
+        self.target_column = target_column
+        self.scalers = {}
+        self.X_train, self.X_test, self.y_train, self.y_test = None, None, None, None
+        self.X_train_seq, self.X_test_seq, self.y_train_seq, self.y_test_seq = None, None, None, None
+        self.logger = logger if logger else logging.getLogger(__name__)
+        self.transformations = []
+        self.lambda_val = None  
+        self.scalers = {
+            "MinMax": MinMaxScaler(),
+            "Standard": StandardScaler(),
+            "Robust": RobustScaler(),
+            "Quantile": QuantileTransformer(output_distribution='normal'),
+            "Power": PowerTransformer(method='yeo-johnson')
+        }
+        self.logger.info("Initializing DataPreprocessor...")        
+    
+    def get_scaler(self, scaler_type: str):
+        self.logger.info(f"Getting scaler of type: {scaler_type}")
+        try:
+            return self.scalers[scaler_type]
+        except KeyError:
+            raise ValueError(f"Invalid scaler_type. Supported types are: {', '.join(self.scalers.keys())}")
+
+    def split_and_plot_data(self, test_size: float = 0.2, split_date: Optional[str] = None, plot: bool = True):
+        self.logger.info("Splitting data...")
+        self.transformations.append('Data Splitting')
+        features = self.data.drop(columns=[self.target_column])
+        target = self.data[self.target_column]
+
+        if split_date:
+            train_mask = self.data.index < split_date
+            self.X_train, self.X_test = features[train_mask], features[~train_mask]
+            self.y_train, self.y_test = target[train_mask], target[~train_mask]
+        else:
+            self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+                features, target, test_size=test_size, shuffle=False
+            )
+
+        self.logger.info(f"Data split completed. X_train shape: {self.X_train.shape}, y_train shape: {self.y_train.shape}")
+
+        if plot:
+            plt.figure(figsize=(20, 7))
+            plt.subplot(1, 2, 1)
+            plt.title('Training Data - Target')
+            plt.plot(self.y_train, label=self.target_column)
+            plt.xlabel("Time")
+            plt.ylabel("Value")
+            plt.legend()
+            plt.subplot(1, 2, 2)
+            plt.title('Test Data - Target')
+            plt.plot(self.y_test, label=self.target_column)
+            plt.xlabel("Time")
+            plt.ylabel("Value")
+            plt.legend()
+            plt.show()
+
+    def normalize_data(self, scaler_type: str = 'MinMax', plot: bool = True):
+        self.logger.info("Normalizing feature data...")
+        scaler = self.get_scaler(scaler_type)
+        self.X_train = scaler.fit_transform(self.X_train)
+        self.X_test = scaler.transform(self.X_test)
+        self.scalers['features'] = scaler
+        self.logger.info("Feature data normalization completed.")
+        self.transformations.append(f"Feature normalization with {scaler_type} scaler")
+
+        if plot:
+            plt.figure(figsize=(20, 8))
+            plt.subplot(1, 2, 1)
+            plt.title('Normalized Training Features')
+            for i in range(self.X_train.shape[1]):
+                plt.plot(self.X_train[:, i], label=f'Feature {i}')
+            plt.legend()
+            plt.subplot(1, 2, 2)
+            plt.title('Normalized Test Features')
+            for i in range(self.X_test.shape[1]):
+                plt.plot(self.X_test[:, i], label=f'Feature {i}')
+            plt.legend()
+            plt.show()
+
+    def normalize_target(self, scaler_type: str = 'MinMax', plot: bool = True):
+        self.logger.info("Normalizing target data...")
+        scaler = self.get_scaler(scaler_type)
+        self.y_train = scaler.fit_transform(self.y_train.values.reshape(-1, 1))
+        self.y_test = scaler.transform(self.y_test.values.reshape(-1, 1))
+        self.scalers['target'] = scaler
+        self.logger.info("Target data normalization completed.")
+        self.transformations.append(f"Target normalization with {scaler_type} scaler")
+
+        if plot:
+            plt.figure(figsize=(20, 7))
+            plt.subplot(1, 2, 1)
+            plt.title('Normalized Training Target')
+            plt.plot(self.y_train, label='Normalized ' + self.target_column)
+            plt.legend()
+            plt.subplot(1, 2, 2)
+            plt.title('Normalized Test Target')
+            plt.plot(self.y_test, label='Normalized ' + self.target_column)
+            plt.legend()
+            plt.show()
+
+    def difference_and_plot_data(self, interval: int = 1, plot: bool = True):
+        self.logger.info(f"Applying differencing with interval {interval}...")
+        self.data = self.data.diff(periods=interval).dropna()
+        self.transformations.append(f'Differencing with interval {interval}')
+        self.logger.info("Differencing applied.")
+        
+        if plot:
+            plt.figure(figsize=(20, 7))
+            plt.title('Data after Differencing')
+            plt.plot(self.data[self.target_column], label=self.target_column)
+            plt.legend()
+            plt.show()
+
+    def box_cox_transform_and_plot(self, lambda_val: Optional[float] = None, plot: bool = True):
+        if self.y_train is None or self.y_test is None:
+            self.logger.warning("Data not split yet. Run split_data first.")
+            return self  # Allow method chaining
+
+        if np.any(self.y_train <= 0) or np.any(self.y_test <= 0):
+            self.logger.warning("Data must be positive for Box-Cox transformation.")
+            return self  # Allow method chaining
+
+        self.logger.info("Applying Box-Cox transformation...")
+        self.y_train = self.y_train.ravel()
+        self.y_test = self.y_test.ravel()
+        self.y_train, fitted_lambda = boxcox(self.y_train)
+        self.lambda_val = fitted_lambda if lambda_val is None else lambda_val
+        self.y_test = boxcox(self.y_test, lmbda=self.lambda_val)
+        self.transformations.append(f"Box-Cox transformation with lambda {self.lambda_val}")
+        self.logger.info(f"Box-Cox transformation applied with lambda {self.lambda_val}.")
+
+        if plot:
+            plt.figure(figsize=(20, 7))
+            plt.subplot(1, 2, 1)
+            plt.title('Box-Cox Transformed Training Target')
+            plt.plot(self.y_train, label='Transformed ' + self.target_column)
+            plt.legend()
+            plt.subplot(1, 2, 2)
+            plt.title('Box-Cox Transformed Test Target')
+            plt.plot(self.y_test, label='Transformed ' + self.target_column)
+            plt.legend()
+            plt.show()
+
+    def inverse_box_cox_and_plot(self, plot: bool = True):
+        if "Box-Cox transformation" not in "".join(self.transformations):
+            self.logger.warning("No Box-Cox transformation found on the target column. Skipping inverse transformation.")
+            return
+
+        self.logger.info("Applying inverse Box-Cox transformation...")
+        self.y_train = invboxcox(self.y_train, self.lambda_val)
+        self.y_test = invboxcox(self.y_test, self.lambda_val)
+        self.transformations.remove(f"Box-Cox transformation with lambda {self.lambda_val}")
+        self.logger.info(f"Inverse Box-Cox transformation applied on column {self.target_column}.")
+        
+        if plot:
+            plt.figure(figsize=(20, 7))
+            plt.subplot(1, 2, 1)
+            plt.title('Inverse Box-Cox Transformed Training Target')
+            plt.plot(self.y_train, label='Inverse Transformed ' + self.target_column)
+            plt.legend()
+            plt.subplot(1, 2, 2)
+            plt.title('Inverse Box-Cox Transformed Test Target')
+            plt.plot(self.y_test, label='Inverse Transformed ' + self.target_column)
+            plt.legend()
+            plt.show()
+
+    def reshape_for_recurrent(self, data: np.array) -> np.array:
+        self.logger.info("Reshaping data for recurrent models...")
+        reshaped_data = data.reshape(data.shape)
+        self.logger.info(f"Data reshaped to {reshaped_data.shape}.")
+        self.transformations.append('Data Reshaped')
+        return reshaped_data
+
+    def generate_sequences(self, X_data: np.array, y_data: np.array, n_steps: int, seq_to_seq: bool = False) -> Tuple[np.array, np.array]:
+        X, y = [], []
+        for i in range(len(X_data) - n_steps):
+            seq_x = X_data[i:i + n_steps, :]
+            if seq_to_seq:
+                seq_y = y_data[i:i + n_steps, :]
+            else:
+                seq_y = y_data[i + n_steps - 1]
+            X.append(seq_x)
+            y.append(seq_y)
+        self.logger.info(f"Generated {len(X)} sequences of shape {X[0].shape}.")
+        self.transformations.append('Sequences Generated')
+        return np.array(X), np.array(y)
+    
+    def prepare_data_for_recurrent(self, n_steps: int, seq_to_seq: bool = False) -> Tuple[np.array, np.array, np.array, np.array]:
+        self.logger.info(f"Preparing data for recurrent models with {n_steps} timesteps...")
+        X_train_seq, y_train_seq = self.generate_sequences(self.X_train, self.y_train, n_steps, seq_to_seq)
+        X_test_seq, y_test_seq = self.generate_sequences(self.X_test, self.y_test, n_steps, seq_to_seq)
+
+        # Update instance variables here
+        self.X_train_seq = self.reshape_for_recurrent(X_train_seq)
+        self.X_test_seq = self.reshape_for_recurrent(X_test_seq)
+        self.y_train_seq = y_train_seq  # Assuming y_train_seq and y_test_seq are already 2D
+        self.y_test_seq = y_test_seq
+
+        self.logger.info("Data preparation for recurrent models completed.")
+        return self.X_train_seq, self.y_train_seq, self.X_test_seq, self.y_test_seq
+
+    def prepare_for_prophet(self) -> pd.DataFrame:
+        prophet_data = self.data[[self.target_column]].reset_index()
+        prophet_data.columns = ['ds', 'y']
+        return prophet_data
+
+    def get_preprocessed_data(self) -> Tuple[np.array, np.array, np.array, np.array]:
+        return self.X_train, self.y_train, self.X_test, self.y_test
+
+    def __str__(self) -> str:
+        return "Transformations applied: " + ", ".join(self.transformations)
